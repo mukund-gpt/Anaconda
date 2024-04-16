82eee981f478a04a4ef9b9a700cdca4073641cd4
Mukund <abhishek.jupyter@gmail.com>
2024-04-16 23:41:11 +0530

    grid-boxes

--- app/src/main/java/com/example/anaconda/SnakeGameScreen.kt
+++ app/src/main/java/com/example/anaconda/SnakeGameScreen.kt
@@ -2,18 +2,28 @@ package com.example.anaconda
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.unit.dp
+import com.example.anaconda.ui.theme.Custard
+import com.example.anaconda.ui.theme.RoyalBlue
+
 
 @Composable
 fun SnakeGameScreen() {
@@ -36,10 +46,38 @@ fun SnakeGameScreen() {
         Canvas(
             modifier = Modifier
                 .fillMaxWidth()
-                .aspectRatio(2/3f)
+                .aspectRatio(2 / 3f)
         ) {
+            val cellSize=size.width/20
+            drawGameBoard(
+                cellSize=cellSize,
+                cellColor = Custard,
+                borderCellColor = RoyalBlue,
+                gridWidth = 20,
+                gridHeight = 30
 
+            )
 
+        }
+        Row (
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ){
+
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "REPLAY")
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "START")
+            }
         }
     }
     
@@ -51,7 +89,17 @@ private fun DrawScope.drawGameBoard(
     borderCellColor: Color,
     gridWidth:Int,
     gridHeight:Int
-
 ){
-
+    for(i in 0 until gridWidth){
+        for (j in 0 until gridHeight){
+            val isBorderCell=(i==0 || j==0 || i==gridWidth-1 || j==gridHeight-1)
+            drawRect(
+                color = if(isBorderCell)borderCellColor
+                        else if((i+j)%2==0)cellColor
+                        else cellColor.copy(alpha = 0.5f),
+                topLeft = Offset(x=i*cellSize,y=j*cellSize),
+                size= Size(cellSize,cellSize)
+            )
+        }
+    }
 }
